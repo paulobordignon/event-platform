f898d623264c71442266960cade0c5aa38a31a40
Paulo Bordignon <p.h_@outlook.com>
2022-08-27 21:49:01 -0300

    feat(all): add graphql request

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
 
+  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
+  
+  console.log(data)
   return (
     <h1 className="text-lg">teste</h1>
   )
